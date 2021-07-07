a99b03bd0b1c3cb5cd5d9cb1984cd7da5d7161b0
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-07-07 16:19:57 +0700

    Show "info" menu in the action bar

--- app/src/main/java/com/example/trustwalletclone/screens/wallets/WalletsFragment.kt
+++ app/src/main/java/com/example/trustwalletclone/screens/wallets/WalletsFragment.kt
@@ -1,11 +1,11 @@
 package com.example.trustwalletclone.screens.wallets
 
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.trustwalletclone.R
 import com.example.trustwalletclone.databinding.WalletsFragmentBinding
@@ -20,7 +20,29 @@ class WalletsFragment : Fragment() {
     ): View? {
         val binding = DataBindingUtil.inflate<WalletsFragmentBinding>(inflater, R.layout.wallets_fragment, container, false)
         viewModel = ViewModelProvider(this).get(WalletsViewModel::class.java)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    private fun getInfoIntent(): Intent {
+        val uri = Uri.parse("https://community.trustwallet.com/t/how-to-import-a-wallet/87")
+        return Intent(Intent.ACTION_VIEW, uri)
+    }
+
+    private fun openInfo() {
+        startActivity(getInfoIntent())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.wallets_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.info -> openInfo()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
