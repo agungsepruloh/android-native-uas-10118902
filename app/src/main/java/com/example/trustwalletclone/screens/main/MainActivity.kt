5dee00673559a93468065a2d6d23fa467cc52687
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-07-07 16:18:49 +0700

    Show back/home button in the action bar

--- app/src/main/java/com/example/trustwalletclone/screens/main/MainActivity.kt
+++ app/src/main/java/com/example/trustwalletclone/screens/main/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.trustwalletclone.screens.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.trustwalletclone.R
 import com.example.trustwalletclone.databinding.ActivityMainBinding
 
@@ -11,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
             R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        showActionBar()
+    }
+
+    private fun showActionBar() {
+        val actionBar: ActionBar? = supportActionBar
+        // Show back button in the action bar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
